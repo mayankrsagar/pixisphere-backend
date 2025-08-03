e914368a9d1ce1561be33683fa69a0ff534ac4f2
mayankrsagar <mayankrsagar@gmail.com>
2025-08-03 14:21:40 +0530

    minor changes

--- services/LeadService.js
+++ services/LeadService.js
@@ -1,6 +1,9 @@
 import Inquiry from '../models/Inquiry.js';
 import User from '../models/User.js';
 
+// Function to assign a partner to an inquiry
+// This function finds the first partner and assigns them to the inquiry
+// It also updates the status of the inquiry to 'in-progress'
 export const assignPartner = async (inquiryId) => {
   const inquiry = await Inquiry.findById(inquiryId);
   const partner = await User.findOne({ role: 'partner' }); // simple: first partner
